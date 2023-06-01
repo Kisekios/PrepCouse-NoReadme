a06bab953ef9b978625990ceab6f0699af0544c3
Kisekios <dekostump@gmail.com>
2023-05-31 19:03:21 -0500

    Test #03 resuelto

---
+++ Enciclopedia.js
@@ -0,0 +1,40 @@
+/// TIPOS DE DATO, OPERADORES , CONDICINALES
+
+/* TIPO DE DATO
+:: string | numero | flotantes | booleanos | undefined | null
+*/
+
+/* OPERADORES
+:: SUMA: + | RESTA: - | MULTIPLICACION: * | DIVISION: / | MODULO: % | POTENCIACION: **
+*/
+
+/*COMPARADORES
+:: MAYOR: > | MENOR: < | MAYOR O IGUAL: >= | MENOR O IGUAL: <= | IGUAL: == O === | DIFERENTE: != | ASIGNACION: = |
+*/
+
+/*CONDICIONALES
+:: IF(#){}ELSE IF(#){}ELSE{}
+
+/// FUNCIONES  ( function(#){return };  | var # = function (){return}; | var # = (x) => { return}; )
+
+/* CONSOLE
+:: console.log() | 
+*/
+
+/// LIBRERIAS
+
+/* MATH
+::MATH.POW(#base,#potencia);
+::MATH.ROUND(# (!redondeaAlEnteroMasCercano) );
+::MATH.FLOOR(# (!redondeaAlEnteroMasBajo) );
+::MATH.CEIL(# (!redondeaAlEnteroProximo) );
+::MATH.MAX(#,#,#,# (!mostraraElNumeroMaximo) );
+::MATH.MIN(#,#,#,# (!mostraraElNumeroMinimo) );
+::MATH.RANDOM( (!numeroAleatorioEntre0Y1) ); 
+*/
+
+
+
+
+
+
